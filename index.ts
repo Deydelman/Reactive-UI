90bfec3ebb1f6a6af711608009ecd81af8e97458
Deydelman <deydelman@usf.edu>
2025-02-11 20:43:44 -0500

    1

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 import express, { Request, Response } from "express";
 
+import path from "path";
+
 const server = express();
 const port = 3000;
 
@@ -21,14 +23,12 @@ server.get("/api/names",
     }
 );
 
-server.get(
-    "/api/weather/:zipcode",
-    function (req: Request, res: Response): void {
-        const response = routeWeather({ zipcode: req.params.zipcode });
-        res.send(response);
-    }
-);
+server.get("/components/weather", function (req: Request, res: Response): void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
 
 server.listen(port, function (): void {
-    console.log("Listening on " + port);
+    console.log("Listening on " + port)
 });
